96e07e9b77b2b0bfffaccd9eea4b7db1e13031dc
aris-creator <56575042+aris-creator@users.noreply.github.com>
2020-12-09 04:54:45 +0000

    Update drawer_user_controller.dart

--- best_flutter_ui_templates/lib/custom_drawer/drawer_user_controller.dart
+++ best_flutter_ui_templates/lib/custom_drawer/drawer_user_controller.dart
@@ -1,5 +1,5 @@
-import 'package:best_flutter_ui_templates/app_theme.dart';
-import 'package:best_flutter_ui_templates/custom_drawer/home_drawer.dart';
+import 'package:belajar_flutter_ui/app_theme.dart';
+import 'package:belajar_flutter_ui/custom_drawer/home_drawer.dart';
 import 'package:flutter/material.dart';
 
 class DrawerUserController extends StatefulWidget {
