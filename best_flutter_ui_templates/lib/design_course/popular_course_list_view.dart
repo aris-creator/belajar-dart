8f7412967e0cafe57295c84e60b630f15bc8409a
aris-creator <56575042+aris-creator@users.noreply.github.com>
2020-12-09 04:46:34 +0000

    Update popular_course_list_view.dart

--- best_flutter_ui_templates/lib/design_course/popular_course_list_view.dart
+++ best_flutter_ui_templates/lib/design_course/popular_course_list_view.dart
@@ -1,6 +1,6 @@
-import 'package:best_flutter_ui_templates/design_course/design_course_app_theme.dart';
-import 'package:best_flutter_ui_templates/design_course/models/category.dart';
-import 'package:best_flutter_ui_templates/main.dart';
+import 'package:belajar_flutter_ui/design_course/design_course_app_theme.dart';
+import 'package:belajar_flutter_ui/design_course/models/category.dart';
+import 'package:belajar_flutter_ui/main.dart';
 import 'package:flutter/material.dart';
 
 class PopularCourseListView extends StatefulWidget {
