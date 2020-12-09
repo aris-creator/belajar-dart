874e78ee084bb40d319ada0f70045e6b29eeec6b
aris-creator <56575042+aris-creator@users.noreply.github.com>
2020-12-09 04:24:10 +0000

    Update calendar_popup_view.dart

--- best_flutter_ui_templates/lib/hotel_booking/calendar_popup_view.dart
+++ best_flutter_ui_templates/lib/hotel_booking/calendar_popup_view.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:best_flutter_ui_templates/hotel_booking/hotel_app_theme.dart';
+import 'package:belajar_flutter_ui/hotel_booking/hotel_app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
