70525adbdcc3f0a3f9ccbd5174294e6397a018d9
aris-creator <56575042+aris-creator@users.noreply.github.com>
2020-12-09 04:18:01 +0000

    Update hotel_home_screen.dart

--- best_flutter_ui_templates/lib/hotel_booking/hotel_home_screen.dart
+++ best_flutter_ui_templates/lib/hotel_booking/hotel_home_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
-import 'package:best_flutter_ui_templates/hotel_booking/calendar_popup_view.dart';
-import 'package:best_flutter_ui_templates/hotel_booking/hotel_list_view.dart';
-import 'package:best_flutter_ui_templates/hotel_booking/model/hotel_list_data.dart';
-import 'package:flutter/material.dart';
+import 'package:belajar_flutter_ui/hotel_booking/calendar_popup_view.dart';
+import 'package:belajar_flutter_ui/hotel_booking/hotel_list_view.dart';
+import 'package:belajar_flutter_ui/hotel_booking/model/hotel_list_data.dart';
+import '/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'filters_screen.dart';
